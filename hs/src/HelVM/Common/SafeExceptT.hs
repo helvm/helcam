57866d26f5728765f8ec212d437552396bd6f555
Kamil Adam <kamil.adam.zabinski@gmail.com>
2021-07-13 15:47:02 +0200

    remove-eveluator

--- hs/src/HelVM/Common/SafeExceptT.hs
+++ hs/src/HelVM/Common/SafeExceptT.hs
@@ -1,22 +1,8 @@
 module HelVM.Common.SafeExceptT (
-  hoistSafe,
-  hoistError,
   unsafeRunExceptT,
 ) where
 
 import HelVM.Common.Safe
-
---except :: Monad m => Either e a -> ExceptT e m a
-hoistSafe :: Monad m => Safe a -> SafeExceptT m a
-hoistSafe = hoistEither
---hoistSafe = except
-
---hoistError :: Monad m => e -> ExceptT e m a
-hoistError :: Monad m => Error -> SafeExceptT m a
-hoistError = hoistSafe . safeError
---hoistError = throwE
-
-----
 
 unsafeRunExceptT :: Monad m => SafeExceptT m a -> m a
 unsafeRunExceptT = fmap unsafe . runExceptT
