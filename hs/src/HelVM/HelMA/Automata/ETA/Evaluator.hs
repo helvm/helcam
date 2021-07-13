ab5377220d0fc8ad6527c02faf66e27612f65e49
Kamil Adam <kamil.adam.zabinski@gmail.com>
2021-07-13 15:37:25 +0200

    remove-eveluator

--- hs/src/HelVM/HelMA/Automata/ETA/Evaluator.hs
+++ hs/src/HelVM/HelMA/Automata/ETA/Evaluator.hs
@@ -18,7 +18,6 @@ import HelVM.HelMA.Automaton.Memories.StackConst as Stack
 import HelVM.HelMA.Automaton.Types.StackType
 
 import HelVM.Common.Safe
-import HelVM.Common.SafeExceptT
 import HelVM.Common.Util
 
 import Prelude hiding (divMod)
@@ -41,42 +40,42 @@ evalTL tl ListStackType = start tl []
 evalTL tl SeqStackType  = start tl Seq.empty
 
 start :: (BIO m , SEvaluator Symbol s m) => TokenList -> s -> m ()
-start il s = liftExceptT $ next (IU il 0) s
+start il s = next (IU il 0) s
 
-next :: SEvaluator e s m => InstructionUnit -> s -> SafeExceptT m ()
-next iu s = doInstruction' =<< hoistSafe (nextIU iu)  where doInstruction' (t , iu') = doInstruction t iu' s
+next :: (BIO m , SEvaluator e s m) => InstructionUnit -> s -> m ()
+next iu s = doInstruction' =<< liftSafe (nextIU iu)  where doInstruction' (t , iu') = doInstruction t iu' s
 
-doInstruction :: SEvaluator e s m => Maybe Token -> InstructionUnit -> s -> SafeExceptT m ()
+doInstruction :: (BIO m , SEvaluator e s m) => Maybe Token -> InstructionUnit -> s -> m ()
 -- IO instructions
 doInstruction (Just O) iu s = doOutputChar iu s
 doInstruction (Just I) iu s = doInputChar  iu s
 
 -- Stack instructions
-doInstruction (Just N) iu s = next' =<< hoistSafe (parseNumber iu) where next' (symbol , iu') = next iu' (push1 symbol s)
-doInstruction (Just H) iu s = next iu =<< hoistSafe (halibut s)
+doInstruction (Just N) iu s = next' =<< liftSafe (parseNumber iu) where next' (symbol , iu') = next iu' (push1 symbol s)
+doInstruction (Just H) iu s = next iu =<< liftSafe (halibut s)
 
 -- Arithmetic
-doInstruction (Just S) iu s = next iu =<< hoistSafe (sub s)
-doInstruction (Just E) iu s = next iu =<< hoistSafe (divMod s)
+doInstruction (Just S) iu s = next iu =<< liftSafe (sub s)
+doInstruction (Just E) iu s = next iu =<< liftSafe (divMod s)
 
 -- Control
 doInstruction (Just R) iu s = next iu s
 doInstruction (Just A) iu@(IU il ic) s = next iu $ flipPush1 s $ genericNextLabel il ic
-doInstruction (Just T) iu@(IU il _ ) s = transfer =<< hoistSafe (pop2 s) where
+doInstruction (Just T) iu@(IU il _ ) s = transfer =<< liftSafe (pop2 s) where
   transfer (_ , 0 , s') = next iu s'
   transfer (0 , _ , _ ) = doEnd iu s
-  transfer (l , _ , s') = next' =<< hoistSafe (genericFindAddress il l) where next' address = next (IU il address) s'
+  transfer (l , _ , s') = next' =<< liftSafe (genericFindAddress il l) where next' address = next (IU il address) s'
 doInstruction Nothing iu s = doEnd iu s
 
 -- IO instructions
-doOutputChar :: SEvaluator e s m => InstructionUnit -> s -> SafeExceptT m ()
-doOutputChar iu s = doOutputChar' =<< hoistSafe (pop1 s) where
-  doOutputChar' (e , s') = hoistMonad (wPutChar (genericChr e)) *> next iu s'
+doOutputChar :: (BIO m , SEvaluator e s m) => InstructionUnit -> s -> m ()
+doOutputChar iu s = doOutputChar' =<< liftSafe (pop1 s) where
+  doOutputChar' (e , s') = (wPutChar (genericChr e)) *> next iu s'
 
-doInputChar :: SEvaluator e s m => InstructionUnit -> s -> SafeExceptT m ()
-doInputChar iu s = doInputChar' =<< hoistMonad wGetChar where
+doInputChar :: (BIO m , SEvaluator e s m) => InstructionUnit -> s -> m ()
+doInputChar iu s = doInputChar' =<< wGetChar where
   doInputChar' char = next iu $ charPush1 char s
 
 -- Terminate instruction
-doEnd :: SEvaluator e s m => InstructionUnit -> s -> SafeExceptT m ()
-doEnd iu s = hoistMonad (wLogShow iu *> wLogShow s)
+doEnd :: (BIO m , SEvaluator e s m) => InstructionUnit -> s -> m ()
+doEnd iu s = wLogShow iu *> wLogShow s
